e8d7c69b05196f13fe1d39c16d15140e86cd7b6b
vinayssmnbr <vinayssmnbr@gmail.com>
2023-03-30 15:58:28 +0530

    add time and greeting on dashboard

--- src/app/dashboard-module/header/header.component.ts
+++ src/app/dashboard-module/header/header.component.ts
@@ -3,8 +3,32 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.css'],
 })
 export class HeaderComponent {
+  date: any;
+greeting:any
+  constructor() {
+  }
+
+  ngOnInit() {
+    const today = new Date();
+    this.date = today.toLocaleString('default', {
+      month: 'long',
+      day: 'numeric',
+      year: 'numeric',
+    });
+
+    const currentHour = today.getHours();
+    if (currentHour < 12) {
+      this.greeting = "Good morning";
+    } else if (currentHour < 18) {
+      this.greeting = "Good afternoon";
+    } else {
+      this.greeting = "Good evening";
+    }
+
+
+}
 
 }
