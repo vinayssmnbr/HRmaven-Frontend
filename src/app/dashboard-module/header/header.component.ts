94af3bacbc72f9ea2dfeb9467e5f75d5366ea4ce
Gursimran Singh <gsb70720@gmail.com>
2023-03-30 15:54:34 +0530

    changes

--- src/app/dashboard-module/header/header.component.ts
+++ src/app/dashboard-module/header/header.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent {
+showSearchBox = false;
 
+  toggleSearchBox() {
+    this.showSearchBox = !this.showSearchBox;
+  }
 }
