603f95e9cae16d44f88c9784bf257e6782996436
Harpreet Singh <harpreet.singh@venturepact.com>
2023-04-01 10:08:12 +0000

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { ForgetComponent } from './forget/forget.component';
 
-const routes: Routes = [{path:'',component:AppComponent},{path:'login',component:LoginComponent},{path:'signup',component:SignupComponent},{ path: 'dashboard', loadChildren: () => import('./dashboard-module/dashboard-module.module').then(m => m.DashboardModuleModule) },{path:'reset',component:ForgetComponent}];
+const routes: Routes = [{path:'',component:AppComponent},{path:'login',component:LoginComponent},{path:'signup',component:SignupComponent},{ path: 'dashboard', loadChildren: () => import('./dashboard-module/dashboard-module.module').then(m => m.DashboardModuleModule) },{path:'reset/:token',component:ForgetComponent}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
