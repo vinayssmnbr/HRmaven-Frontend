449a15d5899df1736d69033ec4c9fb50ee44d21c
vinayssmnbr <vinayssmnbr@gmail.com>
2023-04-01 16:47:38 +0530

    login with google

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,10 @@
 // import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -22,6 +24,10 @@ import { SignupComponent } from './signup/signup.component';
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
+    HttpClientModule,
+    RouterModule,
+
+
 
   ],
   providers: [],
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,12 +1,32 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
-constructor(public fb1:FormBuilder){}
+constructor(public fb1:FormBuilder,private activatedRoute:ActivatedRoute,private http: HttpClient,private router: Router,){}
+
+ngOnInit() {
+  this.activatedRoute.queryParams.subscribe((params) => {
+    // console.log(params);
+    const token = params['token'];
+    console.log(token);
+    if (token) {
+      localStorage.setItem('token', token);
+      this.router.navigate(['dashboard']);
+    }
+  });
+}
+//Google Login
+loginwithGoogle() {
+  window.location.href = 'http://localhost:8000/auth/google';
+}
+
   loginForm = new FormGroup({
     email : new FormControl('',[ Validators.required,Validators.email]),
     password : new FormControl('',[Validators.required,Validators.minLength(5)])
@@ -14,7 +34,7 @@ constructor(public fb1:FormBuilder){}
 
   forgotPassword = new FormGroup({
     email : new FormControl('',[ Validators.required,Validators.email]),
-    
+
   })
 
   showPassword = false;
@@ -35,6 +55,8 @@ constructor(public fb1:FormBuilder){}
   onSubmit(data:any){
 
   }
+
+
 
 
 }
