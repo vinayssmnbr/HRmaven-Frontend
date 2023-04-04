689c2af9928b49e1e6c569e6188a76aad2fe0e47
vinayssmnbr <vinay.kumar@venturepact.com>
2023-04-04 17:01:45 +0530

    a

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,71 +1,78 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { CookieService } from 'ngx-cookie-service';
-import { UserService} from '../service/user.service';
+import { UserService } from '../service/user.service';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
 })
 export class LoginComponent {
-constructor(public fb1:FormBuilder,
-  private activatedRoute:ActivatedRoute,
-  private http: HttpClient,
-  private router: Router,
-  private cookie:CookieService,
-  public userService:UserService
-  ){}
+  constructor(
+    public fb1: FormBuilder,
+    private activatedRoute: ActivatedRoute,
+    private http: HttpClient,
+    private router: Router,
+    private cookie: CookieService,
+    public userService: UserService
+  ) {}
 
-ngOnInit() {
+  ngOnInit() {
+    let counter = 1;
+    setInterval(() => {
+      const radioBtn = document.getElementById(
+        `radio${counter}`
+      ) as HTMLInputElement;
+      radioBtn.checked = true;
+      counter++;
+      if (counter > 4) {
+        counter = 1;
+      }
+    }, 5000);
+    ///
 
-
-  let counter = 1;
-  setInterval(() => {
-    const radioBtn = document.getElementById(`radio${counter}`) as HTMLInputElement;
-    radioBtn.checked = true;
-    counter++;
-    if (counter > 4) {
-      counter = 1;
-    }
-  }, 5000);
-  ///
-
-  this.activatedRoute.queryParams.subscribe((params) => {
-    // console.log(params);
-    const token = params['token'];
-    console.log(token);
-    if (token) {
-      this.cookie.set('token',token);
-      this.router.navigate(['dashboard']);
-    }
-  });
-}
-//Google Login
-loginwithGoogle() {
-  console.log('google');
-  window.location.href = 'http://localhost:3000/auth/google';
-}
-
-
-
+    this.activatedRoute.queryParams.subscribe((params) => {
+      // console.log(params);
+      const token = params['token'];
+      console.log(token);
+      if (token) {
+        this.cookie.set('token', token);
+        this.router.navigate(['dashboard']);
+      }
+    });
+  }
+  //Google Login
+  loginwithGoogle() {
+    console.log('google');
+    window.location.href = 'http://localhost:3000/auth/google';
+  }
 
   loginForm = new FormGroup({
-    email : new FormControl('',[ Validators.required,Validators.email]),
-    password : new FormControl('',[Validators.required,Validators.pattern(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/)])
-  })
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.pattern(
+        /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,}$/
+      ),
+    ]),
+  });
 
   forgotPassword = new FormGroup({
-    email : new FormControl('',[ Validators.required,Validators.email]),
-
-  })
+    email: new FormControl('', [Validators.required, Validators.email]),
+  });
 
   showPassword = false;
   showPasswordIcon = 'fa-eye';
-  Forgotshow=false;
-  EmailSent=false;
-  Invalid=false;
+  Forgotshow = false;
+  EmailSent = false;
+  Invalid = false;
 
   togglePasswordVisibility(passwordInput: any) {
     this.showPassword = !this.showPassword;
@@ -73,81 +80,65 @@ loginwithGoogle() {
     passwordInput.type = this.showPassword ? 'text' : 'password';
   }
 
-
-  toggleForgot(){
-      this.Forgotshow= !this.Forgotshow;
+  toggleForgot() {
+    this.Forgotshow = !this.Forgotshow;
   }
-  toggleForgot1(){
-    this.EmailSent= !this.EmailSent;
-}
+  toggleForgot1() {
+    this.EmailSent = !this.EmailSent;
+  }
 
-closeInvalid(){
-  this.Invalid=!this.Invalid;
-  this.loginForm.reset();
-}
+  closeInvalid() {
+    this.Invalid = !this.Invalid;
+    this.loginForm.reset();
+  }
 
-  loginuser(data: any){
-    this.userService.users(data).subscribe((res:any)=>{
-      this.userService.users(data)
-      console.log("login User: ",res)
+  loginuser(data: any) {
+    this.userService.users(data).subscribe((res: any) => {
+      this.userService.users(data);
+      console.log('login User: ', res);
 
       var today = new Date();
       var expire = new Date();
 
-      expire.setTime(today.getTime() + 3600000*24*15);
-      document.cookie = "name= " + res.Token + ";path=/" + ";expires=" + expire.toUTCString();
-    })
+      expire.setTime(today.getTime() + 3600000 * 24 * 15);
+      document.cookie =
+        'name= ' + res.Token + ';path=/' + ';expires=' + expire.toUTCString();
+    });
   }
-  get email(){
-    return this.forgotPassword.get("email");
+  get email() {
+    return this.forgotPassword.get('email');
   }
 
-submit(){
-  this.router.navigate(['/dashboard'])
+  submit() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  // submissions
+
+  onSubmit(data: any) {
+    console.log(this.loginForm.value);
+    this.userService.users(data).subscribe((res: any) => {
+      this.userService.users(this.loginForm);
+      console.log('login User: ', res);
+      if (res.message == 'login successful') {
+        this.submit();
+      } else if (res.message == 'Invalid') {
+        console.log('haha');
+        this.Invalid = !this.Invalid;
+      }
+    });
+  }
+  ForgetEmailSubmit(data: any) {
+    console.log('Forget Password Email');
+    console.log(data);
+
+    this.userService.ForgotEmail(data).subscribe((res: any) => {
+      this.userService.ForgotEmail(this.forgotPassword);
+      console.log('response:' + res);
+    });
+    this.Forgotshow = !this.Forgotshow;
+    setTimeout(() => {
+      this.EmailSent = !this.EmailSent;
+    }, 1000);
+  }
 }
-
-// submissions
-
-
-onSubmit(data:any){
-  console.log(this.loginForm.value);
-  this.userService.users(data).subscribe((res: any)=>{
-    this.userService.users(this.loginForm)
-    console.log("login User: ", res)
-    if(res.message=="login successful") {
-      this.submit();
-    }
-    else if(res.message=="Invalid"){
-      console.log("haha");
-      this.Invalid=!this.Invalid;
-
-
-    }
-
-  })
-
-}
-ForgetEmailSubmit(data:any)
-{
-  console.log("Forget Password Email");
-  console.log(data);
-
-  this.userService.ForgotEmail(data).subscribe((res:any)=>{
-    this.userService.ForgotEmail(this.forgotPassword);
-    console.log("response:"+res);
-  })
-  this.Forgotshow=!this.Forgotshow;
-  setTimeout(()=>{
-    this.EmailSent=!this.EmailSent;
-
-  },1000);
-
-}
-
-
-
-
-}
-
-
-
