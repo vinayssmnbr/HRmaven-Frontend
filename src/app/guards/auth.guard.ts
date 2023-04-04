3b5f547fc6d9bf22d470718bc04274a3fa52ef04
prince sharma <prince.sharma@venturepact.com>
2023-04-04 15:42:26 +0530

    authguard

--- src/app/guards/auth.guard.ts
+++ src/app/guards/auth.guard.ts
@@ -1,15 +1,21 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree } from '@angular/router';
 import { Observable } from 'rxjs';
+import { UserService } from '../service/user.service';
+import { CookieService } from 'ngx-cookie-service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private userService:UserService, private cookie:CookieService){}
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return true;
+      if(!this.userService.isLoggedIn.value){
+        this.userService.allDataLogin();
+      }
+   return this.userService.isLoggedIn;
   }
-  
+
 }
