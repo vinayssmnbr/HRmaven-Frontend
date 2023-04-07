fa0ff2fb1911a08959aff7a646a4ad85aac98039
Girija <girija.mohanta@venturepact.com>
2023-04-07 14:53:50 +0530

    Dashboard dropdown and three dot

--- src/app/dashboard-module/main/employee-content/employee-content.component.ts
+++ src/app/dashboard-module/main/employee-content/employee-content.component.ts
@@ -1,23 +1,40 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { DashService } from '../../shared/dash.service';
 
 @Component({
   selector: 'app-employee-content',
   templateUrl: './employee-content.component.html',
-  styleUrls: ['./employee-content.component.css']
+  styleUrls: ['./employee-content.component.css'],
 })
 export class EmployeeContentComponent implements OnInit {
   employeeForm: FormGroup;
-  constructor( public dashService:DashService){
+  constructor(public dashService: DashService) {
     dashService.activeComponent = 'employees';
     dashService.headerContent = '';
   }
 
   ngOnInit() {
-   this.employeeForm = new FormGroup({
+    this.employeeForm = new FormGroup({
       employeeId: new FormControl('', Validators.required),
-      designation: new FormControl('', Validators.required)
+      designation: new FormControl('', Validators.required),
+    });
+
+    // ------------Drop Down Menu----------
+    const optionMenu = document.querySelector<HTMLElement>('.select-menu')!,
+      selectBtn = optionMenu.querySelector<HTMLElement>('.select-btn')!,
+      options = optionMenu.querySelectorAll<HTMLElement>('.option'),
+      sBtn_text = optionMenu.querySelector<HTMLElement>('.sBtn-text')!;
+    selectBtn.addEventListener('click', () =>
+      optionMenu.classList.toggle('active')
+    );
+    options.forEach((option) => {
+      option.addEventListener('click', () => {
+        let selectedOption =
+          option.querySelector<HTMLElement>('.option-text')!.innerText;
+        sBtn_text.innerText = selectedOption;
+        optionMenu.classList.remove('active');
+      });
     });
   }
   isDropdownOpen = false;
@@ -25,5 +42,4 @@ export class EmployeeContentComponent implements OnInit {
   toggleDropdown() {
     this.isDropdownOpen = !this.isDropdownOpen;
   }
-
 }
