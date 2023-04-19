a3911b92b7913ddae397a5bd20a3ad82126bf4b4
vinayssmnbr <vinayssmnbr@gmail.com>
2023-04-19 11:13:15 +0530

    no record found after search

--- src/app/dashboard-module/pipe/employeefilter.pipe.ts
+++ src/app/dashboard-module/pipe/employeefilter.pipe.ts
@@ -2,40 +2,56 @@ import { LowerCasePipe } from '@angular/common';
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'employeefilter'
+  name: 'employeefilter',
 })
 export class EmployeefilterPipe implements PipeTransform {
-
   transform(value: any, name: any, designation: any): any {
-    if (name == "") {
+    if (name == '') {
       if (designation == 'Designation' || 'All') {
         return value;
-      }
-      else {
+      } else {
         const filteredArray = [];
         for (let i = 0; i < value.length; i++) {
           if (value[i].designation.toLowerCase() == designation.toLowerCase()) {
             filteredArray.push(value[i]);
           }
         }
+        if (filteredArray.length == 0) {
+          filteredArray.push({ found: 'true' });
+        }
         return filteredArray;
       }
-    }
-    else {
+    } else {
       if (designation == 'Designation' || 'All') {
         const filteredArray = [];
         for (let i = 0; i < value.length; i++) {
-          if (value[i].name.toLowerCase().startsWith(name.toLowerCase()) || value[i].uid.toString().toLowerCase().startsWith(name.toLowerCase())) {
+          if (
+            value[i].name.toLowerCase().startsWith(name.toLowerCase()) ||
+            value[i].uid.toString().toLowerCase().startsWith(name.toLowerCase())
+          ) {
             filteredArray.push(value[i]);
           }
+        }
+        if (filteredArray.length == 0) {
+          filteredArray.push({ found: 'true' });
         }
         return filteredArray;
       } else {
         const filteredArray = [];
         for (let i = 0; i < value.length; i++) {
-          if (value[i].name.toLowerCase().startsWith(name.toLowerCase()) || value[i].uid.toString().toLowerCase().startsWith(name.toLowerCase()) && value[i].designation.toLowerCase() == designation.toLowerCase()) {
+          if (
+            value[i].name.toLowerCase().startsWith(name.toLowerCase()) ||
+            (value[i].uid
+              .toString()
+              .toLowerCase()
+              .startsWith(name.toLowerCase()) &&
+              value[i].designation.toLowerCase() == designation.toLowerCase())
+          ) {
             filteredArray.push(value[i]);
           }
+        }
+        if (filteredArray.length == 0) {
+          filteredArray.push({ found: 'true' });
         }
         return filteredArray;
       }
