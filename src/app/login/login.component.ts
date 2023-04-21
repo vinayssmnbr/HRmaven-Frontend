3c61210a3deaa22a7e941c605e37a6dda5c9775d
Girija <Girija@.com>
2023-04-21 12:08:59 +0530

    Leaves/ Search form add

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -35,19 +35,19 @@ export class LoginComponent {
         Remember: true,
       });
     }
-    let counter = 0;
-    setInterval(() => {
-      const radioBtn = document.getElementById(
-        `radio${counter + 1}`
-      ) as HTMLInputElement;
-      if (radioBtn) {
-        radioBtn.checked = true;
-        counter++;
-        if (counter === 4) {
-          counter = 0;
-        }
-      }
-    }, 8000);
+    // let counter = 0;
+    // setInterval(() => {
+    //   const radioBtn = document.getElementById(
+    //     `radio${counter + 1}`
+    //   ) as HTMLInputElement;
+    //   if (radioBtn) {
+    //     radioBtn.checked = true;
+    //     counter++;
+    //     if (counter === 4) {
+    //       counter = 0;
+    //     }
+    //   }
+    // }, 8000);
     //GOOGLE LOGIN
     this.activatedRoute.queryParams.subscribe((params) => {
       // console.log(params);
