264219d0faa56c0fe22a91931112a2aec5f49c93
Girija <Girija@.com>
2023-04-27 19:05:36 +0530

    employee attendence component

--- src/app/employee-module/main/attendance/attendance.component.ts
+++ src/app/employee-module/main/attendance/attendance.component.ts
@@ -17,6 +17,40 @@ export class AttendanceComponent implements OnInit{
 
 
   }
+  array: any = [
+    {
+      id: 0,
+      name: 'Software Developer',
+    },
+    {
+      id: 1,
+      name: 'Frontend Developer',
+    },
+    {
+      id: 3,
+      name: 'Full Stack Developer',
+    },
+    {
+      id: 4,
+      name: 'UI/UX Designer',
+    },
+  ];
+  contentdropdown: boolean = false;
+  dropdownOpen() {
+  
+    this.contentdropdown = !this.contentdropdown;
+  }
+  Selectvariable: string = 'Designation';
+  colorvariable: number =  0;
+  Changeselect(arr: any) {
+    this.Selectvariable = arr.name;
+    this.colorvariable = arr.id;
+    this.contentdropdown=false;
+    console.log(arr.name);
+  }
+  designationdropdownOption: boolean = false;
 
-
+  dropdownOpenOption() {
+    this.designationdropdownOption = !this.designationdropdownOption;
+  }
 }
