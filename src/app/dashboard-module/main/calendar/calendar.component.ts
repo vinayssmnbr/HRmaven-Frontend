aa48b42a96b38ec3922d69daeaf9620b16ba0c0a
Girija <Girija@.com>
2023-05-04 17:09:54 +0530

    employee leaves

--- src/app/dashboard-module/main/calendar/calendar.component.ts
+++ src/app/dashboard-module/main/calendar/calendar.component.ts
@@ -9,29 +9,35 @@ export class CalendarComponent {
   constructor(public dashService: DashService) {
     dashService.activeComponent = 'calendar';
   }
-  progressBar: any;
-  progressText: any;
-  progress: number = 0;
-  interval: any;
-  ngOnInit() {
-    this.progressBar = document.querySelector('.progress_upload');
-    this.progressText = document.querySelector('.progress-text-input');
+  // progressBar: any;
+  // progressText: any;
+  // progress: number = 0;
+  // interval: any;
+  //  circularProgress: HTMLElement;
+  // progressValue: HTMLElement;
 
-    this.interval = setInterval(() => {
-      this.progress++;
-      if (this.progress > 100) {
-        this.progress = 0;
-      }
-      this.progressBar.style.width = `${this.progress}%`;
-      this.progressText.innerText = `${this.progress}%`;
-    }, 50);
-  
+  // progressStartValue: number = 0;
+  // progressEndValue: number = 90;
+  // speed: number = 100;
+  ngOnInit() {
+    // this.progressBar = document.querySelector('.progress_upload');
+    // this.progressText = document.querySelector('.progress-text-input');
+
+    // this.interval = setInterval(() => {
+    //   this.progress++;
+    //   if (this.progress > 100) {
+    //     this.progress = 0;
+    //   }
+    //   this.progressBar.style.width = `${this.progress}%`;
+    //   this.progressText.innerText = `${this.progress}%`;
+    // }, 50);
+
   }
-  inputfilename: boolean = false;
-  selectedFile: File | null = null;
-  fileName: string = '';
-  onfileselected(event: any) {
-    this.selectedFile = event.target.files[0];
-    this.fileName = this.selectedFile ? this.selectedFile.name : '';
-  }
+  // inputfilename: boolean = false;
+  // selectedFile: File | null = null;
+  // fileName: string = '';
+  // onfileselected(event: any) {
+  //   this.selectedFile = event.target.files[0];
+  //   this.fileName = this.selectedFile ? this.selectedFile.name : '';
+  // }
 }
