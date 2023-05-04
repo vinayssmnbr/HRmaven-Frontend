640206f534572f758bfd48e2bf466669f6fb9dcc
HarpreetSingh <Harpreet.singh@venturepact.com>
2023-05-04 11:19:27 +0530

    solve login bugs

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: false,
 
-  // v1:'https://hrmaven.works/',
-    v1: 'http://localhost:3000/'
+  v1:'https://hrmaven.works/',
+    // v1: 'http://localhost:3000/'
 
 
 };
