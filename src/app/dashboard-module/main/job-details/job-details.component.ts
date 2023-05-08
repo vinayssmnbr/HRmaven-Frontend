7eb12d2d77019f2707e0e24d0ef273c358a29476
Girija <Girija@.com>
2023-05-08 14:06:03 +0530

    job details

--- src/app/dashboard-module/main/job-details/job-details.component.ts
+++ src/app/dashboard-module/main/job-details/job-details.component.ts
@@ -7,45 +7,17 @@ import { DashService } from '../../shared/dash.service';
   styleUrls: ['./job-details.component.css']
 })
 export class JobDetailsComponent {
-  buttonColor2 = '#2F2C9F';
-  buttonbackgroundColor2 = '#ECECEC';
-  buttonColor3 = '#FFFFFF';
-  buttonbackgroundColor3 = '#2F2C9F';
+
   constructor(private dashService:DashService){
     dashService.activeComponent = 'job-details';
     dashService.headerContent = '';
   }
-  firstStep: boolean = true;
-  secondStep: boolean = false;
-  thirdStep: boolean = false;
-  showModal = false;
-  openModal() {
-    this.showModal = true;
-    this.firstStep = true;
-    this.secondStep = false;
-    this.thirdStep = false;
-    // this.showModalContent = true;
+
+  id: any = 'all';
+  tabChange(ids: any) {
+    this.id = ids;
+    console.log(this.id);
   }
-  changeColor2() {
-    this.buttonbackgroundColor2 =
-      this.buttonbackgroundColor2 === '#ECECEC' ? '#2F2C9F' : '#ECECEC';
-    this.buttonColor2 = this.buttonColor2 === '#2F2C9F' ? '#FFFFFF' : '#2F2C9F';
-  }
-  changeColor3() {
-    this.buttonbackgroundColor3 =
-      this.buttonbackgroundColor3 === '#2F2C9F' ? '#FFFFFF' : '#2F2C9F';
-    this.buttonColor3 = this.buttonColor3 === '#FFFFFF' ? '#2F2C9F' : '#FFFFFF';
-  }
-
-
- 
-  visible:boolean =false
-  onclick(){
-
-  this.visible = !this.visible;
-  }
-
-
 
 
 
