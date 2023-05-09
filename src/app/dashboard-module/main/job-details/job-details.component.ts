def471ee1905a434200c8d2065786992eb7b9216
Girija <Girija@.com>
2023-05-09 08:14:37 +0530

    job details modal

--- src/app/dashboard-module/main/job-details/job-details.component.ts
+++ src/app/dashboard-module/main/job-details/job-details.component.ts
@@ -4,11 +4,10 @@ import { DashService } from '../../shared/dash.service';
 @Component({
   selector: 'app-job-details',
   templateUrl: './job-details.component.html',
-  styleUrls: ['./job-details.component.css']
+  styleUrls: ['./job-details.component.css'],
 })
 export class JobDetailsComponent {
-
-  constructor(private dashService:DashService){
+  constructor(private dashService: DashService) {
     dashService.activeComponent = 'job-details';
     dashService.headerContent = '';
   }
@@ -18,9 +17,49 @@ export class JobDetailsComponent {
     this.id = ids;
     console.log(this.id);
   }
+  designationdropdownOption: boolean = false;
 
+  dropdownOpenOption() {
+    this.designationdropdownOption = !this.designationdropdownOption;
+  }
 
+  array: any = [
+    {
+      id: 0,
+      name: 'Shortlisted',
+    },
+    {
+      id: 1,
+      name: 'Hired',
+    },
+    {
+      id: 2,
+      name: 'Interview',
+    },
+    {
+      id: 3,
+      name: 'Rejected',
+    },
+    {
+      id: 4,
+      name: 'Archive',
+    },
+  ];
+  contentdropdown: boolean = false;
+  dropdownOpen() {
+    this.contentdropdown = !this.contentdropdown;
+  }
+  colorvariable: number = 0;
+  Changeselect(arr: any) {
+    this.colorvariable = arr.id;
+    console.log(arr.name);
+  }
 
-
-  
+  Jobdetails: boolean = false;
+  viewbtn(){
+    this.Jobdetails = true;
+  }
+  close_modal(){
+    this.Jobdetails = false;
+  }
 }
