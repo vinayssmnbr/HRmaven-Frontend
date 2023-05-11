d9603473dc50dfa700e445c822c7d750e7837333
HarpreetSingh <Harpreet.singh@venturepact.com>
2023-05-11 16:27:35 +0530

    attendance and leave improve

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,18 +1,10 @@
 export const environment = {
   production: false,
 
-<<<<<<< HEAD
 
   // v1:'https://hrmaven.works/',
-
-
     v1: 'http://localhost:3000/'
 
 
 
-=======
-  v1:'https://hrmaven.works/',
-
-  // v1: 'http://localhost:3000/',
->>>>>>> 5ece7e39a4246f81b15803f36e42cba676e704a6
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,13 +2,6 @@ export const environment = {
   production: true,
   // LIVE SERVER URL
 
-<<<<<<< HEAD
-  v1:'http://localhost:3000/',
+  v1: 'http://localhost:3000/',
   // v1:'https://hrmaven.works/',
-
-}
-=======
-  // v1: 'http://localhost:3000/',
-  v1:'https://hrmaven.works/',
 };
->>>>>>> 5ece7e39a4246f81b15803f36e42cba676e704a6
