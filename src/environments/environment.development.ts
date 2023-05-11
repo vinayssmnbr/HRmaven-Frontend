7a9d429b23bed5df4672150ac67df2c5e1054240
Saacket <saacketpandit417@gmail.com>
2023-05-11 17:00:55 +0530

    emp-dashboard

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -2,8 +2,8 @@ export const environment = {
   production: false,
 
 
-  // v1:'https://hrmaven.works/',
-    v1: 'http://localhost:3000/'
+  v1:'https://hrmaven.works/',
+    // v1: 'http://localhost:3000/'
 
 
 
