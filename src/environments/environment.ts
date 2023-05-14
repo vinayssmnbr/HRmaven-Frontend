5f299f06d5550f94434dfbb1a5049f2a2beec043
HarpreetSingh <Harpreet.singh@venturepact.com>
2023-05-15 01:34:19 +0530

    /

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   // LIVE SERVER URL
 
-  v1: 'http://localhost:3000/',
-  //  v1:'https://hrmaven.works/',
+  // v1: 'http://localhost:3000/',
+   v1:'https://hrmaven.works/',
 };
