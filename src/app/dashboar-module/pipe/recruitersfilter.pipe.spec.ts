bba3306a854c0d388859de26751b71a42dfe90e3
HarpreetSingh <Harpreet.singh@venturepact.com>
2023-05-20 12:52:46 +0530

    recruiment form

---
+++ src/app/dashboar-module/pipe/recruitersfilter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RecruitersfilterPipe } from './recruitersfilter.pipe';
+
+describe('RecruitersfilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RecruitersfilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
---
+++ src/app/dashboard-module/pipe/recruiterfilter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { RecruiterfilterPipe } from './recruiterfilter.pipe';
+
+describe('RecruiterfilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new RecruiterfilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
