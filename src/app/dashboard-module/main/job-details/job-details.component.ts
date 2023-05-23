b22f0049a8615e54adc5ec45cbab2825273426ea
Adarsh3-90 <adarsh.kumar@venturepact.com>
2023-05-24 00:11:03 +0530

    /

--- src/app/dashboard-module/main/job-details/job-details.component.ts
+++ src/app/dashboard-module/main/job-details/job-details.component.ts
@@ -264,41 +264,77 @@ export class JobDetailsComponent {
     //   // this.newcandidateform.reset();
     // });
   }
-  progress: boolean = false;
-  selectedFile: File | null = null;
-  onFileSelected(event: any) {
-    this.selectedFile = event.target.files[0];
+  // progress: boolean = false;
+  // selectedFile: File | null = null;
+  // onFileSelected(event: any) {
+  //   this.selectedFile = event.target.files[0];
+  //   this.fileName = this.selectedFile ? this.selectedFile.name : '';
+  //   this.progress = true;
+  //   this.onUpload(this.selectedFile);
+  // }
+
+  // onUpload(file) {
+  //   console.log('adarsh');
+  //   this.dashService.uploaded(file).then(
+  //     (res) => {
+  //       this.progress = false;
+  //       this.newcandidateform.patchValue({
+  //         url: res && res.url,
+  //       });
+  //     },
+  //     (err) => {
+  //       console.log(err);
+  //       this.progress = false;
+  //     }
+  //   );
+  // }
+
+  progress: boolean = false
+  public selectedFile: File | null = null;
+  fileurl: any;
+
+  async onFileSelected(event: any) {
+    console.log(event.target.value);
+    
+    this.selectedFile = await event.target.files[0];
     this.fileName = this.selectedFile ? this.selectedFile.name : '';
-    this.progress = true;
+    this.progress = true
+    console.log("test11")
     this.onUpload(this.selectedFile);
   }
 
-  onUpload(file) {
-    console.log('adarsh');
-    this.dashService.uploaded(file).then(
-      (res) => {
-        this.progress = false;
-        this.newcandidateform.patchValue({
-          url: res && res.url,
-        });
-      },
-      (err) => {
-        console.log(err);
-        this.progress = false;
-      }
-    );
+  async onUpload(file, changeFile = true) {
+    console.log('adarsh',file);
+    this.selectedPdfFile = file;
+    if(changeFile){
+       return 'file selected'
+    }
+    try{
+      let response = await this.dashService.uploaded(file);;
+      this.progress = false;
+      return response.url;
+    }catch(err){
+      console.log(err);
+      this.progress = false;
+    }
+   
   }
-
   // loading:boolean=false
-  tabChange1() {
+  async tabChange1() {
     let data = {
       ...this.newcandidateform.value,
     };
+    let url= await this.onUpload(this.selectedPdfFile, false)
+    data['url']=url
+   
     this.dashService.addCandidate(data).subscribe((result) => {
       this.dashService.addCandidate(this.newcandidateform);
       // this.newcandidateform.reset();
       // this.loading=false
+    this.fetchJobVecancies();
+
     });
+
     this.newcandidateform.reset();
   }
 
