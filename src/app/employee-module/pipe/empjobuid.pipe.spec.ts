8855768cc77a43c58ceb4d4819cf36b55d906bb9
vinayssmnbr <vinay.kumar@venturepact.com>
2023-05-26 12:28:20 +0530

    filter pipe

---
+++ src/app/employee-module/pipe/empjobuid.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { EmpjobuidPipe } from './empjobuid.pipe';
+
+describe('EmpjobuidPipe', () => {
+  it('create an instance', () => {
+    const pipe = new EmpjobuidPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
